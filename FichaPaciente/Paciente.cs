6adab8e8261b5c77d5aa1655a1a8cd9eb2663754
elayneolima <elayne04lima@gmail.com>
2025-04-25 21:12:17 -0300

    subindo arquivos para o rep remoto

---
+++ FichaPaciente/Paciente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichaPaciente
+{
+    internal class Paciente
+    {
+        public string Nome;
+        public int Idade;
+        public string Diagnostico; 
+
+
+        public Paciente(string nome, int idade, string diagnostico)
+        {
+            this.Nome = nome;  
+            this.Idade = idade;
+            this.Diagnostico = diagnostico;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                   + this.Nome 
+                   + "\n"
+                   + "Idade: "
+                   + this.Idade
+                   +"\n"
+                   +"Diagnóstico:"
+                   + this.Diagnostico;
+        }
+    }
+
+    
+
+    
+
+}
---
+++ FichaPaciente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FichaPaciente;
+
+Console.Write("Nome do Paciente: "); 
+string nome  = Console.ReadLine();
+
+Console.Write("Idade do Paciente: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.Write("Diagnóstico do Paciente: "); 
+string diagnostico = Console.ReadLine();
+
+Paciente pac = new Paciente(nome, idade, diagnostico);
+Console.WriteLine();
+Console.WriteLine("----------Ficha Médica----------");
+Console.WriteLine();
+Console.WriteLine(pac); 
